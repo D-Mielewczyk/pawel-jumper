355fe2f18a39d68275b5361421a3ddb1c69c97d1
Dawid <MDDawid0323@gmail.com>
2023-05-16 22:05:05 +0200

    generate flexible background

--- game/main.py
+++ game/main.py
@@ -1,4 +1,8 @@
+import os
 import pygame
+
+pygame.init()
+os.chdir('..')
 
 WIDTH, HEIGHT = 600, 1100
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -8,27 +12,45 @@ pygame.display.set_caption("Paweł Jumper")
 FPS = 60
 
 
-def draw_window():
-    WIN.fill((52, 91, 235))
+def get_background(color):
+    image = pygame.image.load(f"assets/Background/{color}")
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            cords = (i * width, j * height)
+            tiles.append(cords)
+
+    return tiles, image
+
+
+def draw_window(window, background, background_image):
+    for tile in background:
+        window.blit(background_image, tile)
+
     pygame.display.update()
 
 
-def game_loop():
+def game_loop(window):
     clock = pygame.time.Clock()
-    run = True
 
+    background, background_image = get_background("Blue.png")
+
+    run = True
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                exit()
+                break
             # if event.type == pygame.KEYDOWN:
             #     if event.key == pygame.K_r:
             #         # klawisz r cos robi
-        draw_window()
+        draw_window(window, background, background_image)
     pygame.quit()
+    quit()
 
 
 if __name__ == "__main__":
-    game_loop()
+    game_loop(WIN)
