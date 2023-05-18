fc855bc920f399e2ead5f18620935e7094b8ee63
Dawid <MDDawid0323@gmail.com>
2023-05-18 16:45:50 +0200

    lint

--- game/platform_cls.py
+++ game/platform_cls.py
@@ -5,6 +5,7 @@ from random import randint
 from utils import WIDTH, HEIGHT
 from object import Object
 
+
 def load_platform(width, height):
     path = "assets\Terrain\Terrain (16x16).png"
     image = pygame.image.load(path).convert_alpha()
@@ -13,8 +14,10 @@ def load_platform(width, height):
     surface.blit(image, (0, 0), rect)
     return pygame.transform.scale2x(surface)
 
+
 class Platform(Object):
     next_platform_height = HEIGHT - randint(90, 700)
+
     def __init__(self, x, y, width=96, height=10):
         super().__init__(x, y, width, height)
         self.sprite.blit(load_platform(width, height), (0, 0))
@@ -22,7 +25,12 @@ class Platform(Object):
 
     def gen_platforms(self, max_height, platforms):
         while Platform.next_platform_height >= max_height:
-            platforms.append(Platform(randint(0, WIDTH-self.rect.width), Platform.next_platform_height))
+            platforms.append(
+                Platform(
+                    randint(0, WIDTH - self.rect.width),
+                    Platform.next_platform_height,
+                )
+            )
             Platform.next_platform_height -= randint(0, 625)
 
 
