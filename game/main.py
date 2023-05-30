d80f0ada8a665241f2f141c25b2a94410844bd2e
wojtekszamocki <w.szamocki.02@gmail.com>
2023-05-30 15:11:15 +0200

    Fully functional without animations

--- game/main.py
+++ game/main.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 
 from player import Player
 from platform_cls import Platform
@@ -46,15 +47,24 @@ def draw_window(
     pygame.display.update()
 
 
-def handle_vert_collision(player, *objects):
-    collided = [
-        obj for obj in objects if pygame.sprite.collide_mask(player, obj)
-    ]
+def handle_vert_collision(player, objects):
+    collided = []
+    diss_index = 0
+    for index, obj in enumerate(objects):
+        if pygame.sprite.collide_mask(player, obj):
+            collided.append(obj)
+            if obj.type == "diss":
+                diss_index = index
 
     for obj in collided:
         if player.y_vel > 0:
             player.rect.bottom = obj.rect.top
             player.landed()
+            if obj.type == "tramp":
+                player.y_vel = -25
+            if obj.type == "diss":
+                Platform.disappear(objects, diss_index)
+
 
     return collided
 
@@ -125,6 +135,15 @@ def game_over(window, score):
 def check_loose(player):
     return player.rect.top >= player.dead_height
 
+def platform_type_roll(diff_level):
+    roll = randint (min(diff_level, 50), 100)
+    if 0 <= roll < 70:
+        return "basic"
+    elif 70 <= roll <80:
+        return "tramp"
+    elif 80 <= roll <101:
+        return "diss"
+
 
 def game_loop(window):
     clock = pygame.time.Clock()
@@ -156,7 +175,7 @@ def game_loop(window):
                     player.jump()
 
         player.loop()
-        handle_movement(player, *platforms)
+        handle_movement(player, platforms)
         draw_window(
             window,
             background,
@@ -167,7 +186,11 @@ def game_loop(window):
             *platforms,
         )
 
+<<<<<<< Updated upstream
         offset_y = handle_camera(player, offset_y, platforms)
+=======
+        offset_y = handle_camera(player, offset_y, platforms, score.current_score, platform_type_roll(score.current_score))
+>>>>>>> Stashed changes
 
         score.update_current_score(-offset_y)
 
--- game/platform_cls.py
+++ game/platform_cls.py
@@ -6,11 +6,40 @@ from utils import WIDTH, HEIGHT
 from object import Object
 
 
-def load_platform(width, height):
-    path = "assets\Terrain\Terrain (16x16).png"
+def width_disc(type):
+    switcher = {
+        "basic" : int(96),
+        "tramp" : int(96),
+        "diss"  : int(96),
+    }
+    return switcher.get(type)
+
+def height_disc(type):
+    switcher = {
+        "basic" : int(10),
+        "tramp" : int(24),
+        "diss"  : int(24),
+    }
+    return switcher.get(type)
+
+
+def load_platform(self, width, height):
+    if self.type == "basic":
+        path = "assets\Terrain\Terrain (16x16).png"
+        rect = pygame.Rect(272, 16, width, height)
+    elif self.type == "tramp":
+        path = "assets\Traps\Trampoline\Idle.png"
+        rect = pygame.Rect(2, 17, width, height)
+    elif self.type == "diss":
+        path = "assets\Traps\Platforms\Grey Off.png"
+        rect = pygame.Rect(0, 1, width, height)
     image = pygame.image.load(path).convert_alpha()
     surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+<<<<<<< Updated upstream
     rect = pygame.Rect(272, 0, width, height)
+=======
+    
+>>>>>>> Stashed changes
     surface.blit(image, (0, 0), rect)
     return pygame.transform.scale2x(surface)
 
@@ -18,9 +47,27 @@ def load_platform(width, height):
 class Platform(Object):
     next_platform_height = HEIGHT - randint(90, 700)
 
+<<<<<<< Updated upstream
     def __init__(self, x, y, width=96, height=10):
         super().__init__(x, y, width, height)
         self.sprite.blit(load_platform(width, height), (0, 0))
+=======
+    def __init__(self, x, y, diff_level, type, width = 96, height = 10):
+        self.type = type
+        width = width_disc(type)
+        height = height_disc(type)
+
+        super().__init__(x, y, width, height)
+
+
+        if self.type == "basic":
+            scaled_width = max(96 - 8*(diff_level//15), 48)
+        else:
+            scaled_width = 96
+        original_image = load_platform(self, width, height)
+        scaled_image = pygame.transform.scale(original_image, (scaled_width*2, height*2))
+        self.sprite.blit(scaled_image, (0, 0))
+>>>>>>> Stashed changes
         self.mask = pygame.mask.from_surface(self.sprite)
 
     @staticmethod
@@ -37,6 +84,9 @@ class Platform(Object):
         for index, platform in enumerate(platforms):
             if platform.rect.top > min_height:
                 platforms.pop(index)
+    
+    def disappear(platforms, index):
+        platforms.pop(index)
 
     def reset_platform_height():
         Platform.next_platform_height = HEIGHT - randint(90, 700)
